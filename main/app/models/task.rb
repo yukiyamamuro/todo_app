bbe3226fd3737081c514159ce9e1af6f34aca288
山室 友樹 <yuki.sumaho@gmail.com>
2021-08-05 13:48:36 +0900

    model作成

---
+++ main/app/models/task.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal:true
+
+require Rails.root.join('app/gen/api/todo/todo_pb')
+require Rails.root.join('app/gen/api/todo/todo_services_pb')
+
+class Task
+  include ActiveModel::Model
+
+  class << self
+    def create_todo_task(title)
+      req = Todo::CreateTaskRequest.new({
+        title: title,
+      })
+
+      res = stub.create_todo(req)
+      puts '===========レスポンスが返ってきた==========='
+      puts res.task_id
+    end
+
+    def config_dsn
+      "127.0.0.1:50051"
+    end
+
+    def stub
+      Todo::TodoService::Stub.new(config_dsn, :this_cannel_is_insecure)
+    end
+end
